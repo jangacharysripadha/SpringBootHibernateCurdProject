6eb403d738b240e5bf404c64599b32fbbf5720f5
schar <schar@JANGACHARY>
2023-02-01 12:17:22 +0530

    first commit

---
+++ SpringBootHibernateCurdProject/src/main/java/com/chary/springboot/SpringBootHibernateCurdProjectApplication.java
@@ -0,0 +1,14 @@
+package com.chary.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateCurdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateCurdProjectApplication.class, args);
+		System.out.println("welcome to SpringBoot............");
+	}
+
+}
---
+++ SpringBootHibernateCurdProject/src/main/java/com/chary/springboot/entities/Student.java
@@ -0,0 +1,52 @@
+package com.chary.springboot.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
